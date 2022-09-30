c45618d30e54456a71891648dead88bc5b200a73
Dani <hostnameadmin@protonmail.com>
2022-09-30 14:25:30 +0200

    1. feladat kesz

---
+++ src/Bejegyzes.java
@@ -0,0 +1,53 @@
+import java.time.LocalDateTime;
+
+public class Bejegyzes {
+
+    private String szerzo;
+    private String tartalom;
+    private int likeok;
+    private LocalDateTime letrejott;
+    private LocalDateTime szerkesztve;
+
+    public Bejegyzes(String szerzo, String tartalom) {
+        this.szerzo = szerzo;
+        this.tartalom = tartalom;
+        this.likeok = 0;
+        this.letrejott = LocalDateTime.now();
+        this.szerkesztve = LocalDateTime.now();
+    }
+
+    public String getSzerzo() {
+        return szerzo;
+    }
+
+    public String getTartalom() {
+        return tartalom;
+    }
+
+    public void setTartalom(String tartalom) {
+        this.tartalom = tartalom;
+        this.szerkesztve = LocalDateTime.now();
+    }
+
+    public int getLikeok() {
+        return likeok;
+    }
+
+    public LocalDateTime getLetrejott() {
+        return letrejott;
+    }
+
+    public LocalDateTime getSzerkesztve() {
+        return szerkesztve;
+    }
+
+    public void like(){
+        this.likeok++;
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s–%d–%s \n Szerkeszve: %s \n %s", szerzo, likeok, letrejott, szerkesztve, tartalom);
+    }
+}
